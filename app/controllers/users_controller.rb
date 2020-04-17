d15e3ad6aa1a806b0bf5d2b72a35fa96392b2a24
rishika-shivva <s.rishika2001@gmail.com>
2020-04-17 13:18:43 +0530

    Validate user information before signing up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,27 @@
 class UsersController < ApplicationController
   skip_before_action :ensure_user_logged_in
 
+  def index
+    render "users/new"
+  end
+
   def new
     render "users/new"
   end
 
   def create
-    User.create!(
+    new_user = User.new(
       first_name: params[:first_name],
       last_name: params[:last_name],
       email: params[:email],
       password: params[:password],
     )
-    redirect_to "/"
+    if new_user.save
+      redirect_to new_sessions_path
+      flash[:notice] = "Please Sign in to continue."
+    else
+      flash[:error] = new_user.errors.full_messages.join(", ")
+      redirect_to users_path
+    end
   end
 end
